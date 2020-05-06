d4632b54f6b0f8da8b1a03231d3cd945829c82e9
ryanforstie <ryanforstie@gmail.com>
2020-05-06 16:25:14 -0700

    Add delete company and user method

--- routes/api/company.js
+++ routes/api/company.js
@@ -110,4 +110,21 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @router   DELETE api/company
+// @desc     Delete profile, and user
+// @access   Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // Remove company
+    await Company.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User was deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
