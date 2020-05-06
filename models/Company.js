f9c3c69637004bf1ca7f51a27d2be9a6c286edb9
ryanforstie <ryanforstie@gmail.com>
2020-05-06 14:07:27 -0700

    Add company model with database schema

---
+++ models/Company.js
@@ -0,0 +1,45 @@
+// Company model
+
+const mongoose = require('mongoose');
+
+const CompanySchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  bio: {
+    type: String,
+  },
+  receipts: [
+    {
+      date: {
+        type: Date,
+        required: true,
+      },
+      merchant: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      amount: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
+
+module.exports = Company = mongoose.model('company', CompanySchema);
