e7a8a1081a3efc9879d27fc16695ae2a3afde1d8
ryanforstie <ryanforstie@gmail.com>
2020-05-13 16:22:46 -0700

    Setup Navbar component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,5 +2,24 @@ import React from 'react';
 
 // Functional component
 export const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <a href='index.html'>
+          <i className='fas fa-code'></i> DevConnector
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href='companies.html'>Companies</a>
+        </li>
+        <li>
+          <a href='register.html'>Register</a>
+        </li>
+        <li>
+          <a href='login.html'>Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
