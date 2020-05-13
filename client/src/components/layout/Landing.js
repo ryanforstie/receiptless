421cf2e2d33aada7b6e0db540dd07280d3cb0003
ryanforstie <ryanforstie@gmail.com>
2020-05-13 16:24:14 -0700

    Setup Landing component

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export const Landing = () => {
-  return <div></div>;
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Receiptless</h1>
+          <p className='lead'>Never worry about paper receipts again.</p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
