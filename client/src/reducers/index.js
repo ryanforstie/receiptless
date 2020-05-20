ed63626cdc8bf1dc49e2a7a1f4de0995b38947a6
ryanforstie <ryanforstie@gmail.com>
2020-05-20 09:35:47 -0700

    Setup SET_ALERT reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,3 +1,6 @@
-import { combineReducers } from 'redux';
+// Index reducer
 
-export default combineReducers({});
+import { combineReducers } from 'redux';
+import alert from './alert';
+
+export default combineReducers({ alert });
